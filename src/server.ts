b7da0b87813002dadf65925edf60a172b2c7569d
Jacob Bruce <brucejac@sheridancollege.ca>
2020-10-23 14:13:29 -0400

    Added cors policy

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,12 @@ const app: express.Application = express();
 
 app.use(express.json());
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 app.post('/sendEmail', async (req: express.Request, res: express.Response) => {
     const { name, email, message } = req.body;
     const response: any = await sendEmail(email, name, message);
